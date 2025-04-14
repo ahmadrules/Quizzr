5596617b29f35243eab2f10e630da0fed98b8d14
LilasBe <lilasberakdar1995@gmail.com>
2025-04-14 10:44:43 +0200

    Update FileHandler.java

--- src/model/FileHandler.java
+++ src/model/FileHandler.java
@@ -12,25 +12,40 @@ public class FileHandler{
     public FileHandler() {
 
     }
-    public <T extends Question> List<T> loadQuestions(String fileName, T question) {
+    public <T extends Question> ArrayList<T> loadQuestions(String fileName, T question) {
         List<T> questions = new ArrayList<>();
         StringBuilder questionBuilder = new StringBuilder();
+
         try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line = br.readLine();
             while (line != null) {
-                if (line.isEmpty()) {
+                if (line.trim().isEmpty()) {
                     if (questionBuilder.length()> 0) {
-                        T questionToAdd = (T) question.fromString(questionBuilder.toString());
-                        questions.add(questionToAdd);
+                        String questionString = questionBuilder.toString();
+                        T questionToAdd = (T) question.fromString(questionString);
+                        if (!questionString.isEmpty()) {
+                            questions.add(questionToAdd);}
                         questionBuilder.setLength(0);
                     }
                 }else {
+                    if (questionBuilder.length() > 10000) {
+                        System.out.println("Question builder exceeds 10000 characters");
+                    }
                     questionBuilder.append(line).append("\n");
-                } }
+                }
+                line = br.readLine();
+            }
+            if (questionBuilder.length() > 0) {
+                String questionString = questionBuilder.toString();
+                T questionToAdd = (T) question.fromString(questionString);
+                if (!questionString.isEmpty()) {
+                    questions.add(questionToAdd);
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return questions;
+        return (ArrayList<T>) questions;
     }
 
 }
