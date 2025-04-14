a1d55f64dcd742a324532906ebd563229a93747c
LilasBe <lilasberakdar1995@gmail.com>
2025-04-14 10:49:59 +0200

    Update FlashCard.java

--- src/model/FlashCard.java
+++ src/model/FlashCard.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.io.*;
+
 public class FlashCard {
     private String content;
 
@@ -12,5 +14,27 @@ public class FlashCard {
     public void setContent(String content) {
         this.content = content;
     }
+    public void saveToFile(String filename, Object content) {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(content);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public FlashCard loadFromFile(String filename) {
+        FlashCard flashCard = null;
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename)) ) {
+            flashCard=(FlashCard) ois.readObject();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return flashCard;
+    }
 }
