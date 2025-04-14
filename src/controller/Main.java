3781d9d9e685d707057f0cb57f4afcaeaf22c297
LilasBe <lilasberakdar1995@gmail.com>
2025-04-14 10:34:43 +0200

    Update Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -13,22 +13,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-       // QuizzrExample quiz = new QuizzrExample();
-        //SwingUtilities.invokeLater(quiz);
-       // MultipleChoice multipleChoice= new MultipleChoice("What is the next month", Collections.singletonList("june;july,May"),"May",5);
-       // System.out.println(multipleChoice);
-        //test test
-       // multipleChoice.saveToFile("Test", multipleChoice);
-       // multipleChoice.readFromFile("Test");
-     //   System.out.println(multipleChoice);
-        String filename= "testLoading.txt";
-        FileHandler fileHandler = new FileHandler();
-        MultipleChoice mc= new MultipleChoice("",new ArrayList<>(), "", 0);
-        List<MultipleChoice> multipleChoices = fileHandler.loadQuestions(filename,mc);
-        for (MultipleChoice mc2 : multipleChoices) {
-            System.out.println(mc2);
-        }
-        //test test
 
     }
 }
