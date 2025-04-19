37842a6dff6834494ebc435a05b06b253a5b4f29
LilasBe <lilasberakdar1995@gmail.com>
2025-04-19 19:20:04 +0200

    deleted innehållet i main

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -15,47 +15,7 @@ public class Main {
         new Controller();
         //test test
 
-        //test test
-       // FileHandler fileHandler = new FileHandler();
-        //List<Question> questions1 = new ArrayList<>();
-        String filePath1= "src/model/files/course2/module1/trueFalse_questions.txt";
-       /** TrueOrFalse tf= new TrueOrFalse("", Collections.singletonList(""),0,"");
-        questions1=fileHandler.loadQuestions(filePath1,tf);
-        for (Question question : questions1) {
-            System.out.println(question);
-        }*/
-      String filePath2= "src/model/files/course2/module1/multiChoice_questions.txt";
-     /**  List<MultipleChoice> multipleChoices = new ArrayList<>();
-       MultipleChoice multipleChoice= new MultipleChoice("", Collections.singletonList(""),"",5);
-       ArrayList<Question> questions2= fileHandler.loadQuestions(filePath2,multipleChoice);
-        for (Question question : questions2) {
-            System.out.println(question);
-        }*/
-       String filePath3= "src/model/files/course2/module1/matching_questions.txt";
-       /** List<Matching> questions3 = new ArrayList<>();
-        HashMap<String,Integer> matches = new HashMap<>();
-        Matching matching= new Matching("",Collections.singletonList(""),Collections.singletonList(""),0,matches);
-        questions3= fileHandler.loadQuestions(filePath3, matching);
-        for (Question question : questions3) {
-            System.out.printf("Question: %s\n",question);
-        }*/
-        Module module1 = new Module("module1");
-        //ArrayList<Question> matchingQuestions =module1.generateMatchingQuiz(filePath3,5);
-      /**  for (Question question : questions2) {
-            System.out.println(question);
-        }*/
-       //ArrayList<Question> trueFalseQuestions = module1.generateTrueOrFalseQuiz(filePath1,8);
-        /**for (Question question : questions1) {
-            System.out.println(question);
-        }*/
-     // ArrayList<Question> questions1 = module1.generateMultipleChoiceQuiz(filePath,10);
-     /** for (Question question : questions1) {
-          System.out.println(question);
-      }**/
-     ArrayList<Question> questions= module1.generateGeneralQuiz(filePath2,filePath3,filePath1,21);
-     for (Question question : questions) {
-         System.out.println(question);
-     }
+
 
     }
 }
