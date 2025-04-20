4d131d7869f23a3d4438230630d7b6a189852114
LilasBe <lilasberakdar1995@gmail.com>
2025-04-20 09:19:50 +0200

    Update Quiz.java

--- src/model/Quiz.java
+++ src/model/Quiz.java
@@ -8,10 +8,18 @@ public class Quiz implements Serializable {
     private List<Question> questions;
     private List<String> userAnswers;
     private String name;
+    private int result;
 
     public Quiz(String name) {
         this.questions = new ArrayList<>();
         this.name = name;
+        this.result = 0;
+    }
+    public void setResult(int result) {
+        this.result = result;
+    }
+    public int getResult() {
+        return result;
     }
     public int CalculateScore(){
         int sum = 0;
