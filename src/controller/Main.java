5f68e804be3801e9aa92fb1c1cd07c94eba17cd2
LilasBe <lilasberakdar1995@gmail.com>
2025-04-23 17:41:06 +0200

    updated matching files

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -4,14 +4,10 @@ import model.*;
 import model.Module;
 import view.main.MainFrame;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 
 public class Main {
     public static void main(String[] args) {
-       //new Controller();
+       new Controller();
 
     }
 }
