d012e719a958476ff8e7893162673d7cf1d9bfcb
LilasBe <lilasberakdar1995@gmail.com>
2025-04-29 21:46:07 +0200

    Javadoc fileHandler

--- src/model/FileHandler.java
+++ src/model/FileHandler.java
@@ -4,11 +4,28 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class used to read files
+ * @author Lilas Beirakdar
+ */
 public class FileHandler implements Serializable {
-
+    /**
+     * a constructor to create an object of the filehandler class
+     * @author Lilas Beirakdar
+     */
     public FileHandler() {
-
     }
+
+    /**
+     * Method used to read a String question from a file and change it into a Question object
+     * It can read all types of Strings and return a list of question objects
+     * @param fileName the name of the file containing the questions
+     * @param question the type of the question we want to read it can be a TrueOrFalse Question,
+     * MultipleChoice or a Matching question
+     * @return List of Question objects
+     * @param <T> generic type representing any class inheriting from the class Question
+     * @author Lilas Beirakdar
+     */
     public <T extends Question> ArrayList<T> loadQuestions(String fileName, T question) {
         List<T> questions = new ArrayList<>();
         StringBuilder questionBuilder = new StringBuilder();
