8ef1cab2c6506b459125903456804f19d7148b9e
LilasBe <lilasberakdar1995@gmail.com>
2025-06-06 09:52:20 +0200

    Update Hasher.java

--- src/model/Hasher.java
+++ src/model/Hasher.java
@@ -8,6 +8,7 @@ import java.security.NoSuchAlgorithmException;
  * and provide a certain security level
  */
 public class Hasher {
+
     /**
      * Takes a String and returs its SHA-256 hash
      * @param input The text is to be hashed
@@ -15,7 +16,6 @@ public class Hasher {
      * @throws RuntimeException if SHA-256 is not supported
      * @author Lilas Beirakdar
      */
-
     public static String hash(String input) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -25,7 +25,6 @@ public class Hasher {
                 hexString.append(String.format("%02x", b));
             }
             return hexString.toString();
-
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
